cd170e6f9c018c06a9b46f1d8af6cb9186051cff
yangqiong <364727769@qq.com>
2019-11-28 20:42:41 +0800

    1.rabbitmq使用fanout 路由方式发送消息

---
+++ 06-rabbitmq-fanout-provider/src/test/java/yq/test/QueueTest.java
@@ -0,0 +1,31 @@
+package yq.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import yq.App;
+import yq.sender.Sender;
+
+/**
+ * 消息队列测试类
+ * @author Administrator
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= App.class)
+public class QueueTest {
+
+	@Autowired
+	private Sender sender;
+	
+	/*
+	 * 测试消息队列
+	 */
+	@Test
+	public void test1(){
+		this.sender.send("Hello RabbitMQ");
+	}
+}
