e8040ea25251d9732d0732dd6ac3eb421f6ec6d7
yangqiong <364727769@qq.com>
2019-11-28 12:01:06 +0800

    1.健康状态检查服务端和客户端 2.访问页面是服务端的访问地址

---
+++ 02-springboot-admin-server/src/main/java/yq/App.java
@@ -0,0 +1,13 @@
+package yq;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
