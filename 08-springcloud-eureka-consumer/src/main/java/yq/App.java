70bd62a3db5a6d0c29f00b8ffe852fd6b7fad412
yangqiong <364727769@qq.com>
2019-11-29 14:35:22 +0800

    1.更改rabbin的负载均衡策略

--- 08-springcloud-eureka-consumer/src/main/java/yq/App.java
+++ 08-springcloud-eureka-consumer/src/main/java/yq/App.java
@@ -1,12 +1,20 @@
 package yq;
 
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class App {
+    //修改rabbin的负债均衡策略
+    @Bean
+    public RandomRule createRule()
+    {
+        return new RandomRule();
+    }
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
     }
