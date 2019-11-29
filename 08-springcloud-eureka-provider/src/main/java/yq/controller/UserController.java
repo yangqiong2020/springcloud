569b85c55d6e0cdd5881652b80b4e10baeb4931a
yangqiong <364727769@qq.com>
2019-11-29 11:38:40 +0800

    1.在eureka服务中增加提供服务方和消费服务方，注意服务名不要冲突 当服务挂掉之后，eureka会保存挂掉的服务，当调用的时候，可能还会调用到挂掉的服务，导致报错。 要关闭掉eureka的保护机制，然后优雅的关闭服务

---
+++ 08-springcloud-eureka-provider/src/main/java/yq/controller/UserController.java
@@ -0,0 +1,22 @@
+package yq.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import yq.pojo.User;
+
+@RestController
+public class UserController {
+
+	@RequestMapping("/user")
+	public List<User> getUsers(){
+		List<User> list = new ArrayList<>();
+		list.add(new User(1,"zhangsan",20));
+		list.add(new User(2,"lisi",22));
+		list.add(new User(3,"wangwu",20));
+		return list;
+	}
+}
