b022fee05e6e34cc84a42d438ef8645aa2e234a3
yangqiong <364727769@qq.com>
2019-12-02 15:09:51 +0800

    1.访问方式：http://网关服务地址:网关服务端口/访问的服务的名称/访问的服务中的接口的地址   例如：http://localhost:9020/e-book-order-provider/order/findAll

---
+++ 21-zuul-dateway/src/main/java/App/ZuulApplication.java
@@ -0,0 +1,17 @@
+package App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+}
