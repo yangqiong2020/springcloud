acdd47656d1664c5d1996a1e0f79e6e6342122e6
yangqiong <364727769@qq.com>
2019-12-03 17:32:50 +0800

    1.trace:从客户端发起请求（request）抵达被追踪的边界开始，到北追踪系统向客户端返回响应（response）为止的整个过程     2.span:每个trace中会调用若干个服务，为了记录调用了哪些服务，以及每次调用服务的消耗时间等信息，在每次调用服务时，埋入一个调用记录     3.x-b3-parentSpanid：标识当前工作单元所属的上一个工作单元     4.x-b3-sampled:是否采样，1表示需要被输出,0表示不需要被输出     5.x-b3-traceid:一条请求链路trace的唯一标识，必须值     6.x-span-name:工作单元的名称，例如：http://user/login     7.x-b3-spanid：一个工作单元span的唯一标识，必须值

---
+++ 26-sleuth-consumer/src/main/java/com/book/consumer/controller/ConsumerController.java
@@ -0,0 +1,29 @@
+package com.book.consumer.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.book.consumer.service.ConsumerProductService;
+import com.book.product.pojo.Product;
+
+/**
+ * 创建订单
+ * @author Administrator
+ *
+ */
+@RestController
+public class ConsumerController {
+
+	@Autowired
+	private ConsumerProductService productService;
+	
+
+	@RequestMapping(value="/find",method=RequestMethod.GET)
+	public List<Product> createOrder(){
+		List<Product> products = this.productService.findAll();
+		return products;
+	}
+	
+}
